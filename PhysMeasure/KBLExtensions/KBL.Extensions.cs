70a003c6eac50b4bf9f7ca92142dea1b551e8407
KiloBravo <KiloBravoLima@users.noreply.github.com>
2016-06-05 02:44:20 +0000

    Split out Exensions to KBL.Extensions.dll

--- PhysMeasure/KBLExtensions/KBL.Extensions.cs
+++ PhysMeasure/KBLExtensions/KBL.Extensions.cs
@@ -5,11 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 
-namespace KBL.Extensions
+namespace System
 {
     public static class DoubleExtensions
     {
-        public static int EpsilonCompareTo(this double thisValue, double otherValue)
+        public static Int32 EpsilonCompareTo(this Double thisValue, Double otherValue)
         {   /* Limited precision handling */
             double RelativeDiff = (thisValue - otherValue) / thisValue;
             if (RelativeDiff < -1e-15)
@@ -24,6 +24,74 @@ namespace KBL.Extensions
         }
     }
 
+    public static class StringExtensions
+    {
+
+        public static String Tail(this String text, int Length = 1)
+        {
+            int FullLen = text.Length;
+            return text.Substring(FullLen - Length , Length);
+        }
+
+        public static String Head(this String text, int Length = 1)
+        {
+            return text.Substring(0, Length);
+        }
+
+
+        public static List<String> Chop(this String text, int maxLength = 80)
+        {
+            List<String> textParts = new List<String>();
+            int textLen = text.Length;
+            int choppedLen = 0;
+
+            while (choppedLen < textLen)
+            {
+                int len = textLen - choppedLen;
+                if (len > maxLength)
+                {
+                    len = maxLength; // Make extra line shift
+
+                    // Wrap whole word to next line; find start of word to wrap
+                    while (len > 0 && text[choppedLen + len - 1] != ' ' && text[choppedLen + len] != ' ')
+                    {
+                        len--;
+                    }
+
+                    if (len == 0)
+                    {   // One large word on this line; Can't wrap whole word to next line. Must output something to this line. 
+                        len = maxLength;
+                    }
+                }
+                String strtext;
+                try
+                {
+                    // strtext =  "| " + choppedLen.ToString() + " " + len.ToString() + " |"   + text.Substring(choppedLen, len);
+                    strtext = text.Substring(choppedLen, len);
+                }
+                catch
+                {
+                    strtext = "| Exception: " + choppedLen.ToString() + " " + len.ToString() + " |";
+                }
+
+                textParts.Add(strtext);
+                choppedLen += len;
+            }
+
+            return textParts;
+        }
+    }
+
+    public static class DateTimeExtensions
+    {
+        public static string ToSortString(this DateTime Me) => Me.ToString("yyyy-MM-dd HH:mm:ss");
+
+        public static string ToSortShortDateString(this DateTime Me) => Me.ToString("yyyy-MM-dd");
+    }
+}
+
+namespace System.Collections.Generic 
+{
 
     public static class IEnumerableExtensions
     {
@@ -57,6 +125,12 @@ namespace KBL.Extensions
                 return item;
             return null;
         }
+
+        public static String ToStringList<Object>(this IEnumerable<Object> sequence, String separator = ", ") 
+        {
+            return sequence.Select(item => item.ToString()).Aggregate((current , next) => current + separator + next);
+        }
+
     }
 
 
@@ -104,60 +178,12 @@ namespace KBL.Extensions
             return null;
         }
     }
+}
 
-
-    public static class StringExtensions
+namespace System.Reflection
+{
+    public static class DateTimeBuildNo
     {
-        public static List<String> Chop(this String text, int maxLength = 80)
-        {
-            List<String> textParts = new List<String>();
-            int textLen = text.Length;
-            int choppedLen = 0;
-
-            while (choppedLen < textLen)
-            {
-                int len = textLen - choppedLen; 
-                if (len > maxLength) 
-                {
-                    len = maxLength; // Make extra line shift
-
-                    // Wrap whole word to next line; find start of word to wrap
-                    while (len > 0 && text[choppedLen + len - 1] != ' ' && text[choppedLen + len] != ' ')
-                    {
-                        len--;
-                    }
-
-                    if (len == 0)
-                    {   // One large word on this line; Can't wrap whole word to next line. Must output something to this line. 
-                        len = maxLength;
-                    }
-                }
-                String strtext;
-                try
-                {
-                    // strtext =  "| " + choppedLen.ToString() + " " + len.ToString() + " |"   + text.Substring(choppedLen, len);
-                    strtext = text.Substring(choppedLen, len);
-                }
-                catch
-                {
-                    strtext = "| Exception: " + choppedLen.ToString() + " " + len.ToString() + " |";
-                }
-
-                textParts.Add(strtext);
-                choppedLen += len;
-            }
-
-            return textParts;
-        }
-    }
-
-
-    public static class DateTimeSortString
-    {
-        public static string ToSortString(this DateTime Me) => Me.ToString("yyyy-MM-dd HH:mm:ss");
-
-        public static string ToSortShortDateString(this DateTime Me) => Me.ToString("yyyy-MM-dd");
-
         public static void ToBuildNo(this DateTime Me, out int buildNo, out int revisionNo)
         {
             TimeSpan TimeSince_2000_01_01 = Me.Date - new DateTime(2000, 1, 1);
@@ -165,8 +191,21 @@ namespace KBL.Extensions
             buildNo = TimeSince_2000_01_01.Days;
             revisionNo = (int)(Me.TimeOfDay.TotalSeconds / 2);
         }
-    }
 
+        public static DateTime FromBuildNo(int buildNo, int revisionNo)
+        {
+            DateTime buildDateTime = new DateTime(2000, 1, 1).Add(new TimeSpan(TimeSpan.TicksPerDay * buildNo +             // days since 1 January 2000
+                                                                               TimeSpan.TicksPerSecond * 2 * revisionNo));  // seconds since midnight, (multiply by 2 to get original)
+            return buildDateTime;
+        }
+
+        public static String ToBuildDateString(int buildNo, int revisionNo)
+        {
+            DateTime buildDateTime = FromBuildNo(buildNo, revisionNo);
+            String buildNoString = buildDateTime.ToSortString();
+            return buildNoString;
+        }
+    }
 
     public static class AssemblyExtensions
     {
@@ -179,9 +218,8 @@ namespace KBL.Extensions
 
             if (AsemVersion.Build != 0)
             {
-                DateTime buildDateTime = new DateTime(2000, 1, 1).Add(new TimeSpan(TimeSpan.TicksPerDay * AsemVersion.Build +             // days since 1 January 2000
-                                                                                    TimeSpan.TicksPerSecond * 2 * AsemVersion.Revision));  // seconds since midnight, (multiply by 2 to get original)
-                InfoStr = String.Format("{0} {1}", AsemVersion.ToString(), buildDateTime.ToSortString());
+                String buildNoString = DateTimeBuildNo.ToBuildDateString(AsemVersion.Build, AsemVersion.Revision);
+                InfoStr = String.Format("{0} {1}", AsemVersion.ToString(), buildNoString);
             }
             else
             {
@@ -193,15 +231,13 @@ namespace KBL.Extensions
         public static String AssemblyFileVersionInfo(this System.Reflection.Assembly assembly)
         {
             FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
-            //String AsemVersion = String.Format("{0}.{1}.{2}.{3}", fileVersionInfo.FileMajorPart, fileVersionInfo.FileMinorPart, fileVersionInfo.FileBuildPart, fileVersionInfo.FilePrivatePart);
             String AsemVersion = fileVersionInfo.FileVersion;
             String InfoStr;
 
             if (fileVersionInfo.FileBuildPart != 0)
             {
-                DateTime buildDateTime = new DateTime(2000, 1, 1).Add(new TimeSpan(TimeSpan.TicksPerDay * fileVersionInfo.FileBuildPart +             // days since 1 January 2000
-                                                                                    TimeSpan.TicksPerSecond * 2 * fileVersionInfo.FilePrivatePart));  // seconds since midnight, (multiply by 2 to get original)
-                InfoStr = String.Format("{0} {1}", AsemVersion, buildDateTime.ToSortString());
+                String buildNoString = DateTimeBuildNo.ToBuildDateString(fileVersionInfo.FileBuildPart, fileVersionInfo.FilePrivatePart);
+                InfoStr = String.Format("{0} {1}", AsemVersion, buildNoString);
             }
             else
             {
@@ -212,11 +248,8 @@ namespace KBL.Extensions
 
         public static String AssemblyInfo(this System.Reflection.Assembly assembly)
         {
-            // System.Reflection.Assembly assembly = typeof(Quantity).Assembly;
             System.Reflection.AssemblyName AsmName = assembly.GetName();
 
-            //FileInfo AsmFileInfo = new FileInfo(Asm.Location);
-            // Version AsemVersion = AsmName.Version;
             String assemblyVersionInfo = AssemblyVersionInfo(assembly);
             String assemblyFileVersionInfo = AssemblyFileVersionInfo(assembly);
 
